d28b718e62fc9f6bbbdbdcd04825e7ac084a79dc
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-09-19 10:15:46 +0300

    Login page
    add login.blade.php view
    make login page the default view

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+@extends('base')
+
+@section("content")
+<div class="container" style="height:500px;padding-top:100px;">
+    <div class="row">
+        <div class="col-sm-4 mx-auto">
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <button type="submit" class="btn btn-secondary">Login</button>
+            </form>
+        </div>
+    </div>
+
+</div>
+
+@endsection
